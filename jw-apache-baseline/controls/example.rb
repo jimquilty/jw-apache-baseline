5abb01ad7e3cdd3f2def41f2a7c138e79d395f12
Jim Quilty <jquilty@chef.io>
2020-07-31 10:41:24 -0400

    Initial commit
    Signed-off-by: Jim Quilty <jquilty@chef.io>

---
+++ jw-apache-baseline/controls/example.rb
@@ -0,0 +1,6 @@
+# copyright: 2018, The Authors
+
+title "Apache Baseline Wrapper"
+
+include_controls 'apache-baseline'
+
---
+++ jw-apache-baseline/habitat/plan.sh
@@ -0,0 +1,8 @@
+pkg_name=apache-baseline-effortless
+pkg_version=1.0.0
+pkg_origin=chef
+pkg_scaffolding="chef/scaffolding-chef-inspec"
+scaffold_automate_server_url='https://192.168.42.106'                                   # Example: https://foo.bar.com
+scaffold_automate_user='admin'                                    # Example: "admin"
+scaffold_automate_token='03XQwyOiNVfw2L_kDfisrwCtYaw='                                        # Example: "DI0WVxInnyGnWKRlZBGizTXySgk="
+scaffold_compliance_insecure=true  
